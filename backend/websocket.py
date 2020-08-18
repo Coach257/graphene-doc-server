57839fbbc1c4a718ebe547598da68f09c91dde7a
Coach257 <shizhelun20000820@126.com>
2020-08-18 17:53:29 +0800

    websocket 2.0

--- backend/websocket.py
+++ backend/websocket.py
@@ -8,19 +8,25 @@ from geventwebsocket.websocket import WebSocket   #这条做语法提示用
 
 app = Flask(__name__)
 
+
 users = set()
 @app.route('/conn')
 def index():
     wsock = request.environ.get('wsgi.websocket')
+    print(wsock)
     users.add(wsock)
     while True:
         message = wsock.receive()
-        print(message)
         if message:
+            obj=json.loads(message)
+            username=obj['username']
+            content=obj['content']
+            res="{\"username\":\""+username+"\",\"content\":\""+content+"\"}"
             for user in users:
                 if user!=wsock:
                     try:
-                        user.send(message)
+                        print(res)
+                        user.send(res)
                     except:
                         print("用户断开连接") 
                         users.remove(wsock)
@@ -30,6 +36,6 @@ def index():
 if __name__ == '__main__':
     # app.run()
     #在APP外封装websocket
-    http_serv = WSGIServer(("127.0.0.1",8888),app,handler_class=WebSocketHandler)
+    http_serv = WSGIServer(("0.0.0.0",8888),app,handler_class=WebSocketHandler)
     # 启动服务
     http_serv.serve_forever()
